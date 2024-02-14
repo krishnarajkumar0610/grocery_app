6ac520c00613e727d6808f6093d0e00f583e39ba
krishnaraj0610@gmail.com <krishnaraj0610@gmail.com>
2024-02-15 02:23:12 +0530

    Grocery App

---
+++ grocery_app/lib/screens/logins/sign_in.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../home_page.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final _username = TextEditingController();
+  final _signInPass = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: [
+      Column(
+        children: [
+          Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 20.0, top: 30),
+              child: TextField(
+                controller: _username,
+                decoration: InputDecoration(
+                    hintText: "Full Name",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 20.0, top: 30),
+              child: TextField(
+                controller: _signInPass,
+                decoration: InputDecoration(
+                    hintText: "Full Name",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              )),
+          Padding(
+            padding: const EdgeInsets.only(top: 30.0),
+            child: GestureDetector(
+              onTap: () {
+                // when i sign up then it need to display a box to tell go to sign in page
+                _username.dispose();
+
+                _signInPass.dispose();
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ));
+              },
+              child: Container(
+                width: 250,
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.orange,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 2,
+                        offset: const Offset(0, 3),
+                      ),
+                    ]),
+                child: Center(
+                  child: Text(
+                    "Sign In",
+                    style: GoogleFonts.notoSerif(
+                        fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 190.0, top: 10),
+            child: Text(
+              "Forget password?",
+              style: GoogleFonts.notoSerif(
+                  fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Text(
+              "--------------or Sign In with--------------",
+              style: GoogleFonts.notoSerif(
+                  fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                    width: 120,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Google",
+                        style: GoogleFonts.notoSerif(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.black),
+                      ),
+                    )),
+                Container(
+                    width: 120,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[900],
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Facebook",
+                        style: GoogleFonts.notoSerif(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.white),
+                      ),
+                    )),
+              ],
+            ),
+          )
+        ],
+      ),
+    ]);
+  }
+}
---
+++ grocery_app/lib/screens/logins/sign_up.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../home_page.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final _fullName = TextEditingController();
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: [
+      Column(
+        children: [
+          Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 20.0, top: 30),
+              child: TextField(
+                controller: _fullName,
+                decoration: InputDecoration(
+                    hintText: "Full Name",
+                    prefixIcon: const Icon(Icons.person),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 20.0, top: 30),
+              child: TextField(
+                controller: _email,
+                decoration: InputDecoration(
+                    hintText: "Mail id",
+                    prefixIcon: const Icon(Icons.mail),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 20.0, top: 30),
+              child: TextField(
+                controller: _password,
+                decoration: InputDecoration(
+                    hintText: "Password",
+                    prefixIcon: const Icon(Icons.lock),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20))),
+              )),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: GestureDetector(
+              onTap: () {
+                // when i sign up then it need to display a box to tell go to sign in page
+                _fullName.dispose();
+                _email.dispose();
+                _password.dispose();
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomePage(),
+                    ));
+              },
+              child: Container(
+                width: 250,
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.orange,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 3,
+                        blurRadius: 2,
+                        offset: const Offset(0, 3),
+                      ),
+                    ]),
+                child: Center(
+                  child: Text(
+                    "Sign Up",
+                    style: GoogleFonts.notoSerif(
+                        fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "--------------or Sign up with--------------",
+              style: GoogleFonts.notoSerif(
+                  fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                    width: 120,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Google",
+                        style: GoogleFonts.notoSerif(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.black),
+                      ),
+                    )),
+                Container(
+                    width: 120,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[900],
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Facebook",
+                        style: GoogleFonts.notoSerif(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.white),
+                      ),
+                    )),
+              ],
+            ),
+          )
+        ],
+      ),
+    ]);
+  }
+}
