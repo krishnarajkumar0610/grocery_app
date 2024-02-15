9da7b7a31e81dc4f4c6860f4c2f1a7398e3e122d
krishnaraj0610@gmail.com <krishnaraj0610@gmail.com>
2024-02-15 06:38:35 +0530

    Grocery App

--- grocery_app/lib/screens/pages/cart_page.dart
+++ grocery_app/lib/screens/pages/cart_page.dart
@@ -16,67 +16,70 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orangeAccent,
-        centerTitle: true,
-        title: Text(
-          "My Cart",
-          style: GoogleFonts.notoSerif(
-              fontWeight: FontWeight.bold, color: Colors.black),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.lightGreenAccent,
+          centerTitle: true,
+          title: Text(
+            "My Cart",
+            style: GoogleFonts.notoSerif(
+                fontWeight: FontWeight.bold, color: Colors.black),
+          ),
         ),
-      ),
-      body: BlocConsumer<GroceryBloc, GroceryStates>(
-        builder: (context, state) {
-          // Your UI based on state goes here
-          return state.cartItems!.isEmpty
-              ? const Center(
-                  child: Icon(
-                  Icons.remove_shopping_cart,
-                  size: 200,
-                ))
-              : ListView.builder(
-                  itemCount: state.cartItems!.length!,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(20)),
-                        child: ListTile(
-                            leading: Image.asset(state.cartItems![index][2]),
-                            title: Text(
-                              state.cartItems![index][0],
-                              style: GoogleFonts.notoSerif(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),
-                            ),
-                            subtitle: Text(
-                              "Rs.${state.cartItems![index][1]}",
-                              style: GoogleFonts.notoSerif(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),
-                            ),
-                            trailing: IconButton(
-                              onPressed: () {
-                                print("INSIDE CART : ${state.themeStatus}");
-                                context.read<GroceryBloc>().add(RemoveItem(
-                                      index: index,
-                                    ));
-                                //Navigator.pop(context);
-                              },
-                              icon:
-                                  const Icon(Icons.cancel, color: Colors.black),
-                            )),
-                      ),
-                    );
-                  },
-                );
-        },
-        listener: (context, state) {
-          // Handle events or side effects here
-        },
+        body: BlocConsumer<GroceryBloc, GroceryStates>(
+          builder: (context, state) {
+            // Your UI based on state goes here
+            return state.cartItems!.isEmpty
+                ? const Center(
+                    child: Icon(
+                    Icons.remove_shopping_cart,
+                    size: 200,
+                  ))
+                : ListView.builder(
+                    itemCount: state.cartItems!.length!,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(20)),
+                          child: ListTile(
+                              leading: Image.asset(state.cartItems![index][2]),
+                              title: Text(
+                                state.cartItems![index][0],
+                                style: GoogleFonts.notoSerif(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                              subtitle: Text(
+                                "Rs.${state.cartItems![index][1]}",
+                                style: GoogleFonts.notoSerif(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                              trailing: IconButton(
+                                onPressed: () {
+                                  print("INSIDE CART : ${state.themeStatus}");
+                                  context.read<GroceryBloc>().add(RemoveItem(
+                                        index: index,
+                                      ));
+                                  //Navigator.pop(context);
+                                },
+                                icon:
+                                    const Icon(Icons.cancel, color: Colors.black),
+                              )),
+                        ),
+                      );
+                    },
+                  );
+          },
+          listener: (context, state) {
+            // Handle events or side effects here
+          },
+        ),
       ),
     );
   }
